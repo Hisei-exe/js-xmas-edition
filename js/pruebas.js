ac20ca07a7031f14c30236b95dc9cb7dcd97c1fd
Hisei-exe <60720710+Hisei-exe@users.noreply.github.com>
2021-12-07 02:04:45 -0300

    Clase 8 + Tarea

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,66 @@
 function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
-
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
+    console.assert(
+        validarNombre('') === 'Este campo debe tener al menos 1 carácter.',
+        'Validar nombre no validó que el nombre no sea vacío.'
+    );
+    console.assert(
+        validarNombre(
+            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
+        ) === 'Este campo debe tener menos de 50 caracteres.',
+        'Validar nombre no validó que el nombre sea menor a 50 caracteres.'
+    );
+    console.assert(
+        validarNombre('123123123') === 'El nombre debe tener sólo letras.',
+        'Validar nombre no validó que el nombre tenga sólo letras.'
+    );
+    console.assert(
+        validarNombre('Matias') === '',
+        'Validar nombre no funcionó con un nombre valido.'
+    );
 }
-
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad('') === 'Debe seleccionar una ciudad.',
+        'Validar ciudad no validó que una ciudad esté seleccionada.'
+    );
+    console.assert(
+        validarCiudad('Salta') === '',
+        'Validar ciudad no funcionó con una ciudad seleccionada.'
+    );
+}
+function probarValidarComportamiento() {
+    console.assert(
+        validarComportamiento('') === 'Debe seleccionar un comportamiento.',
+        'Validar comportamiento no validó que un comportamiento esté seleccionado.'
+    );
+    console.assert(
+        validarComportamiento('maso') === '',
+        'Validar comportamiento no funcionó con un comportamiento seleccionado.'
+    );
+}
+function probarValidarDescripcionRegalo() {
+    console.assert(
+        validarDescripcionRegalo('') ===
+            'La descripción debe tener al menos 1 carácter.',
+        'Validar descripción no validó que la descripción tenga al menos 1 carácter.'
+    );
+    console.assert(
+        validarDescripcionRegalo(
+            'asdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklqasdfghjklq'
+        ) === 'La descripción debe tener menos de 100 caracteres.',
+        'Validar descripción no validó que la descripción tenga menos de 100 caracteres.'
+    );
+    console.assert(
+        validarDescripcionRegalo('"#$"#%"#%"') ===
+            'La descripción debe contener sólo letras y números.',
+        'Validar descripción no validó que la descripción contenga sólo letras y números.'
+    );
+    console.assert(
+        validarDescripcionRegalo('Regalo regalin') === '',
+        'Validar descripción no funcionó con una descripción valida.'
+    );
+}
 probarValidarNombre();
+probarValidarCiudad();
+probarValidarComportamiento();
+probarValidarDescripcionRegalo();
--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -1,7 +1,91 @@
 /*
-* Hacer las funciones de validación de validarCiudad y validarDescripcionRegalo.
-* Escribir pruebas para esas funciones.
-*
-* Adicional: Escribir pruebas para las funciones de tareas anteriores.
-*
-* */
+ * Hacer las funciones de validación de validarCiudad y validarDescripcionRegalo.
+ * Escribir pruebas para esas funciones.
+ *
+ * Adicional: Escribir pruebas para las funciones de tareas anteriores.
+ *a
+ * */
+
+function validarFormulario(event) {
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const comportamiento = $form.comportamiento.value;
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+    const errores = {
+        nombre: validarNombre(nombre),
+        ciudad: validarCiudad(ciudad),
+        comportamiento: validarComportamiento(comportamiento),
+        'descripcion-regalo': validarDescripcionRegalo(descripcionRegalo),
+    };
+    manejarErrores(errores);
+    event.preventDefault();
+}
+
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
+        return 'Este campo debe tener al menos 1 carácter.';
+    }
+    if (nombre.length >= 50) {
+        return 'Este campo debe tener menos de 50 caracteres.';
+    }
+    if (!/^[A-z]+$/.test(nombre)) {
+        return 'El nombre debe tener sólo letras.';
+    }
+    return '';
+}
+function validarCiudad(ciudad) {
+    if (ciudad === '') {
+        return 'Debe seleccionar una ciudad.';
+    }
+    return '';
+}
+function validarComportamiento(comportamiento) {
+    if (comportamiento === '') {
+        return 'Debe seleccionar un comportamiento.';
+    }
+    return '';
+}
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length === 0) {
+        return 'La descripción debe tener al menos 1 carácter.';
+    }
+    if (descripcionRegalo.length >= 100) {
+        return 'La descripción debe tener menos de 100 caracteres.';
+    }
+    if (!/^[A-z0-9\s]+$/.test(descripcionRegalo)) {
+        return 'La descripción debe contener sólo letras y números.';
+    }
+    return '';
+}
+function manejarErrores(errores) {
+    let contadorErrores = 0;
+    Object.keys(errores).forEach(function (key) {
+        contadorErrores += errores[key].length;
+        const errorEnLaLista = document.querySelector(`#error-${key}`);
+        if (errores[key]) {
+            $form[key].className = 'error';
+            $form[key].value = '';
+            if (!errorEnLaLista) {
+                const errorLista = document.createElement('li');
+                errorLista.innerText = errores[key];
+                errorLista.id = `error-${key}`;
+                document.querySelector('#errores').appendChild(errorLista);
+            }
+        } else {
+            $form[key].className = '';
+            if (errorEnLaLista) {
+                errorEnLaLista.remove();
+            }
+        }
+    });
+    if (contadorErrores === 0) {
+        document.formulario.className = 'oculto';
+        document.querySelector('#exito').className = '';
+        setTimeout(function () {
+            window.location.href = 'wishlist.html';
+        }, 5000);
+    }
+}
+
+const $form = document.formulario;
+$form.onsubmit = validarFormulario;
